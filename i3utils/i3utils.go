87a339471b10b19c392f9e5ff172cf610d53fef1
Moritz <moritz@zendri.com>
2022-07-29 22:28:01 +0200

    Remove print statement

--- i3utils/i3utils.go
+++ i3utils/i3utils.go
@@ -14,7 +14,6 @@ func GenerateNeoVideWmClassForDir(directory string) string {
 
 func MoveToWorkspace(selector string, workspace i3.Workspace) ([]i3.CommandResult, error) {
 	command := fmt.Sprintf("[%s] move to workspace %v", selector, workspace.Name)
-	fmt.Println(command)
 	return i3.RunCommand(command)
 }
 func MoveToWorkspaceByWmClass(wmClass string, workspace i3.Workspace) ([]i3.CommandResult, error) {
